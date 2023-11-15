4d6dbfba6ed84c83a6d40793b7be0410eca98a0b
Belotskie <150890870+Belotskie@users.noreply.github.com>
2023-11-15 15:19:19 +0800

    Update streamlit_app.py
    update header title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.write('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
 streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('KAle, Spinach & Rocket Smoothie')
+streamlit.write('Kale, Spinach & Rocket Smoothie')
 streamlit.write('Hard-Boiled Free-Range Egg')
