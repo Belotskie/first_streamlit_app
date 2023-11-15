db8d72d015f3ec18b6b4f97f3ae8997e2807688b
Belotskie <150890870+Belotskie@users.noreply.github.com>
2023-11-15 15:14:29 +0800

    Update streamlit_app.py
    Add header and body info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.body('Omega 3 & Blueberry Oatmeal')
+streamlit.body('KAle, Spinach & Rocket Smoothie')
+streamlit.body('Hard-Boiled Free-Range Egg')
